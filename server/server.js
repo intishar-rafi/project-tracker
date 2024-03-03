3b94879f8cb2204b4a25442380d04b0b1f7fe32d
Mashook-Mohammad_Meshkat <mashook.mohammad.meshkat@g.bracu.ac.bd>
2024-03-04 04:23:23 +0600

    Try

--- server/server.js
+++ server/server.js
@@ -6,14 +6,9 @@ const dbConfig = require("./config/dbConfig");
 const port = process.env.PORT || 5000;
 
 const usersRoute = require("./routes/usersRoute");
-const projectsRoute = require("./routes/projectsRoute");
-const tasksRoute = require("./routes/tasksRoute");
-const notificationsRoute = require("./routes/notificationsRoute");
 
 app.use("/api/users", usersRoute);
-app.use("/api/projects", projectsRoute);
-app.use("/api/tasks", tasksRoute);
-app.use("/api/notifications", notificationsRoute);
+
 
 // deployment config
 const path = require("path");
